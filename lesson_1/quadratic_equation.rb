18fdfaa1ac2d864703b4ccbe59c0b4685c4af7cb
oda <olen2000@yandex.ru>
2022-03-05 14:11:17 +0900

    Lesson 1 - fixed code style

--- lesson_1/quadratic_equation.rb
+++ lesson_1/quadratic_equation.rb
@@ -12,9 +12,9 @@ d = b * b - 4 * a * c
 if d < 0
   puts 'Корней нет'
 else
-	sqrt = Math.sqrt(d)
-	x1 = (-b + sqrt) / 2 * a
-	x2 = (-b - sqrt) / 2 * a
+  sqrt = Math.sqrt(d)
+  x1 = (-b + sqrt)/2 * a
+  x2 = (-b - sqrt)/2 * a
 
-	puts x1 == x2 ? "x = #{x1}" : "x1 = #{x1}, x2 = #{x2}"
+  puts x1 == x2 ? "x = #{x1}" : "x1 = #{x1}, x2 = #{x2}"
 end
--- lesson_1/right_triangle.rb
+++ lesson_1/right_triangle.rb
@@ -5,13 +5,13 @@ triangle = []
   triangle << gets.to_f
 end
 
-triangle.sort! {|a, b| b <=> a}
+triangle.sort! { |a, b| b <=> a }
 
 if triangle.uniq.size == 1
   puts 'Треугольник равносторонний и равнобедренный'
 elsif triangle.uniq.size == 2
   puts 'Треугольник равнобедренный'
-elsif triangle[0] ** 2 == triangle[1] ** 2 + triangle[2] ** 2
+elsif triangle[0]**2 == triangle[1]**2 + triangle[2]**2
   puts 'Треугольник прямоугольный'
 else
   puts 'Треугольник обычный'
